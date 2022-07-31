f74023c3a03f4ca262ebbb107a78d7055eba2ee4
junxiangke <junxiangk@163.com>
2022-08-01 00:24:36 +0800

    revised test0731_2

--- test0731_2.py
+++ test0731_2.py
@@ -12,6 +12,7 @@ image_width = 800
 image_height = int(image_width / aspect_ratio)
 canvas = ti.Vector.field(3, dtype=ti.f32, shape=(image_width, image_height))
 hit_point_flag = ti.field(dtype=ti.i32, shape=(image_width, image_height))
+hit_point_coor = ti.Vector.field(3, dtype=ti.f32, shape=(image_width, image_height))
 
 
 @ti.kernel
@@ -26,6 +27,7 @@ def ray_intersect():
         if flag > 0:
             canvas[i, j] = ti.Vector([1.0, 1.0, 1.0])
             hit_point_flag[i, j] = 1
+            hit_point_coor[i, j] = intersect_z_plane_point(ray.origin, ray.direction, 0, xy_range)
 
 
 @ti.func
@@ -45,16 +47,40 @@ def intersect_z_plane(ray_o, ray_d, z_plane, xy_range):
             flag = -1
     return flag
 
+@ti.func
+def intersect_z_plane_point(ray_o, ray_d, z_plane, xy_range):
+    # ray_o and ray_d shape (3, )
+    # z_plane value
+    # xy_range (xmin, y_min, x_max, y_max)
+    flag = 1
+    # hit_point = np.zeros(3)
+    res = ti.Vector([0.0, 0.0, 0.0])
+    if ray_d[2] < 1e-6:
+        flag = -1
+    else:
+        t = (z_plane - ray_o[2]) / ray_d[2]
+        hit_point = ray_o + t * ray_d
+        if hit_point[0] < xy_range[0] or hit_point[0] > xy_range[1] \
+            or hit_point[1] < xy_range[2] or hit_point[1] > xy_range[3]:
+            flag = -1
+        else:
+            res = hit_point
+    return res
+
 
 if __name__ == "__main__":
     camera = Camera()
     gui = ti.GUI("Ray Tracing", res=(image_width, image_height))
     canvas.fill(0)
     hit_point_flag.fill(0)
+    hit_point_coor.fill(0)
     ray_intersect()
-    while gui.running:
-        gui.set_image(canvas.to_numpy())
-        gui.show()
+    # while gui.running:
+    #     gui.set_image(canvas.to_numpy())
+    #     gui.show()
     hit_point_flag_numpy = hit_point_flag.to_numpy()
     hit_point_flag_numpy = np.rot90(hit_point_flag_numpy)
-    cv2.imshow("image", hit_point_flag_numpy)
+    idx_x, idx_y = np.where(hit_point_flag_numpy == 1)
+    print(len(idx_x))
+    
+    # cv2.imshow("image", hit_point_flag_numpy)
